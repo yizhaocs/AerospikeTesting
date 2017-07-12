836cad9ff07fb17117c6dabd7de5ad87c54e8977
yzhao <yi.zhao@adara.com>
2017-07-12 16:24:46 -0700

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,13 @@ import com.aerospike.client.Bin;
 import com.aerospike.client.Key;
 import com.aerospike.client.Record;
 import com.aerospike.client.policy.ClientPolicy;
-import model.CookieData;
+import com.opinmind.bidgen.CookieRouter;
+import com.opinmind.ssc.CookieData;
+import com.opinmind.ssc.cache.RemoteUserDataCacheImplV3;
+import com.opinmind.ssc.cache.UserDataCacheFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * build it:
@@ -15,11 +21,12 @@ import model.CookieData;
  */
 public class Main {
     public static void main(String[] args){
-        connection();
+        //String cookieId = args[0];
+        connection("1234");
     }
 
 
-    public static void connection(){
+    public static void connection(String cookieId){
         ClientPolicy policy = null;
         AerospikeClient client = null;
 
@@ -28,6 +35,7 @@ public class Main {
             client = new AerospikeClient(policy, "172.28.128.3", 3000);
             getPutOperations_test(client);
             getPutOperations_adara(client);
+            getPutOperations_adara_prod(client,cookieId);
         }finally {
             if(client != null){
                 client.close();
@@ -49,6 +57,7 @@ public class Main {
 
     public static void getPutOperations_adara(AerospikeClient client){
         Key key = new Key("adara", "CookieData", "putgetkey");
+
         CookieData mCookieData = new CookieData();
         Bin bin1 = new Bin("bin1", mCookieData);
         Bin bin2 = new Bin("bin2", mCookieData);
@@ -56,5 +65,70 @@ public class Main {
         client.put(null, key, bin1, bin2);
         Record record = client.get(null, key);
         System.out.println(record.toString());
+    }
+
+    public static void getPutOperations_adara_prod(AerospikeClient client, String cookieId){
+        Key key = new Key("adara", "CookieData", "putgetkey");
+        RemoteUserDataCacheImplV3 userDataCacheBDB = null;
+
+        try {
+            userDataCacheBDB = getUserDataCacheBDB();
+        }catch(Exception e){
+            System.out.println();
+            e.printStackTrace();
+        }
+        CookieData mCookieData = null;
+
+        try {
+            mCookieData = userDataCacheBDB.getCookieRawData(Long.valueOf(cookieId));
+        }catch(Exception e){
+            System.out.println("[getPutOperations_adara_prod error]");
+            e.printStackTrace();
+        }
+
+        if(mCookieData != null){
+            Bin bin1 = new Bin("bin1", mCookieData);
+            Bin bin2 = new Bin("bin2", mCookieData);
+
+            client.put(null, key, bin1, bin2);
+            Record record = client.get(null, key);
+            System.out.println(record.toString());
+        }else{
+            System.out.println("CookieData is null");
+        }
+
+
+    }
+
+    private static RemoteUserDataCacheImplV3 getUserDataCacheBDB()
+            throws NumberFormatException, Exception {
+
+        CookieRouter cookieRouter = new CookieRouter();
+        cookieRouter.setNodes("localhost:8080");
+
+        List<String> configFileList = new ArrayList<String>();
+        configFileList.add("/opt/opinmind/conf/common.properties");
+        configFileList.add("/opt/opinmind/conf/local.properties");
+        configFileList.add("/opt/opinmind/conf/bidgen.nodes.properties");
+        configFileList.add("/opt/opinmind/conf/credentials/passwords.properties");
+        cookieRouter.setConfigFile(configFileList);
+        cookieRouter.init();
+
+
+        int maxItemListLength = 10000000;
+        String maxItemListLengthStr = "100000";
+        if (maxItemListLengthStr != null && maxItemListLengthStr.length() > 0) {
+            maxItemListLength = Integer.valueOf(maxItemListLengthStr);
+        }
+
+        RemoteUserDataCacheImplV3 userDataCache = null;
+
+        userDataCache = (RemoteUserDataCacheImplV3) UserDataCacheFactory
+                .createRemoteUserDataCache(cookieRouter,
+                        maxItemListLength, true);
+        userDataCache.setBidgenCacheNodes("localhost:8080");
+        userDataCache.init();
+
+        return userDataCache;
     }
 }
